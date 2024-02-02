f65fabbbb0aa785cc4327bdef6a216cdaad9f57f
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2024-02-02 12:31:49 +0700

    Update concac.lua

--- concac.lua
+++ concac.lua
@@ -1,112 +1,52 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-_G.Team = "Pirate" -- Marine / Pirate
-_G.KAITUN_SCRIPT = true
-_G.LogsDes = {
-    ["Enabled"] = false, -- ?????????????
-    ["SendAlias"] = true, -- ?????????? Alias
-    ["SendDescription"] = true, -- ?????????? Des
-    ["DelaySend"] = 5 -- ??????
-}
-_G.WebHook = {
-    ["Enabled"] = false, -- ?????????????
-    ["Url"] = "", -- ?????????????
-    ["Delay"] = 60 -- ??????
-}
-_G.MainSettings = {
-        ["EnabledHOP"] = false, -- ???? HOP ( ??????????????????????? )
-        ['FPSBOOST'] = true, -- ??????
-        ["FPSLOCKAMOUNT"] = 30, -- ????? FPS
-        ['WhiteScreen'] = true, -- ?????
-        ['CloseUI'] = false, -- ??? Ui
-        ["NotifycationExPRemove"] = true, -- ?? ExP ????????????????
-        ['AFKCheck'] = 1500, -- ?????????????????????????????????
-        ["LockFragments"] = 20000, -- ????????????
-	["LockFruitsRaid"] = { -- ??????????????????????
-            [1] = "Dough-Dough",
-            [2] = "Leopard-Leopard",
-            [3] = "T-Rex-T-Rex",
-            [4] = "Dragon-Dragon",
-            [5] = "Kitsune-Kitsune",
-            [6] = "Mammoth-Mammoth",
-        }
-    }
-_G.Fruits_Settings = { -- ?????????
-    ['Main_Fruits'] = {''}, -- ?????? ?????????????????????????????????????????????????
-    ['Select_Fruits'] = {""} -- ?????????????????????
-}
-_G.Quests_Settings = { -- ????????????????
-    ['Rainbow_Haki'] = true,
-    ["MusketeerHat"] = true,
-    ["PullLever"] = false,
-    ['DoughQuests_Mirror'] = {
-        ['Enabled'] = true,
-        ['UseFruits'] = false
-    }        
-}
-_G.Races_Settings = { -- ???????????
-    ['Race'] = {
-        ['EnabledEvo'] = false,
-        ["v2"] = true,
-        ["v3"] = true,
-        ["Races_Lock"] = {
-            ["Races"] = { -- Select Races U want
-                ["Mink"] = true,
-                ["Human"] = true,
-                ["Fishman"] = true
-            },
-            ["RerollsWhenFragments"] = 20000 -- Random Races When Your Fragments is >= Settings
-        }
-    }
-}
-_G.Settings_Melee = { -- ???????????
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.FarmMastery_Settings = {
-    ['Melee'] = true,
-    ['Sword'] = false,
-    ['DevilFruits'] = false,
-    ['Select_Swords'] = {
-        ["AutoSettings"] = false, -- ??????????????????????????????????????????????????????
-        ["ManualSettings"] = { -- ??????? AutoSettings ???? false ??????????????????????????? ????????????????
-            "CursedDualKatana"
-            
-        }
-    }
-}
-_G.SwordSettings = { -- ??????????
-    ['Saber'] = true,
-    ["Pole"] = false,
-    ['MidnightBlade'] = false,
-    ['Shisui'] = false,
-    ['Saddi'] = false,
-    ['Wando'] = false,
-    ['Yama'] = true,
-    ['Rengoku'] = false,
-    ['Canvander'] = false,
-    ['BuddySword'] = false,
-    ['TwinHooks'] = false,
-    ['HallowScryte'] = true,
-    ['TrueTripleKatana'] = false,
-    ['CursedDualKatana'] = true
-}
-_G.GunSettings = { -- ??????????
-    ['Kabucha'] = false,
-    ['SerpentBow'] = false,
-    ['SoulGuitar'] = true
+repeat wait() until game:IsLoaded()
+_G["Fumo Blox Fruit - HoHo Hub Kaitun V3"] = {
+    MAIN_UI_COLOR = Color3.fromRGB(255, 165, 0), -- Color3.fromRGB(170, 85, 255)
+    
+    BlackScreen = true, -- less lag
+    FastMode = true, -- true/false less lag
+    AutoRejoinGame = 1800, -- time in second, rejoin to bypass anticheat
+    GetFruitInWorld = true, -- true/false collect fruit
+    AutoTeam = "Pirate", -- Pirate/Marine
+    GetMelees = {"Superhuman", "Death Step","Sharkman Karate","Electric Claw","Dargon Talon", "Godhuman"}, -- remove if u dont wanna get what melee
+    Redeem_X2EXP_on = 10, -- to redeem all x2 exp code on what level
+    Character_Config = {
+        InvisFromKen = true, -- true/false anyone use ken cant see your name
+        NoStun = true, -- true/false make u dont get stuned when farming
+    },
+    ListFruitTake = {
+        "',
+        -- put fruit you want like above, recommend fruit can awake
+        -- fruit list: 'Sand-Sand','Smoke-Smoke','Ice-Ice','Kilo-Kilo','Diamond-Diamond','Light-Light','Shadow-Shadow','Portal-Portal','Spider-Spider','Gravity-Gravity','Spin-Spin','Revive-Revive','Bird-Bird: Phoenix','Rubber-Rubber','Spring-Spring','Blizzard-Blizzard','Dough-Dough','Bomb-Bomb','Venom-Venom','Bird-Bird: Falcon','Spirit-Spirit','Love-Love','Leopard-Leopard','Dragon-Dragon','Spike-Spike','Magma-Magma','Control-Control','Flame-Flame','Rumble-Rumble','Dark-Dark','Chop-Chop','Quake-Quake','Buddha-Buddha','Barrier-Barrier',"Rocket-Rocket","Sound-Sound","Pain-Pain","Mammoth-Mammoth","Kitsune-Kitsune","T-Rex-T-Rex"
+    },
+    BuyBetterFruit = false, --true/false if u alr have fruit but better fruit stock it will buy it
+    EatFruitWhenSea3 = false, --true/false we need to keep fruit before third sea
+    AwakeFruit = false, --true/false awake after a raid
+    AutoBuy = {
+        Shop_Items = true, --true/false auto buy all item sell with beli like Bisento,... we will buy when max level
+        All_Haki = false, --true/false ken,buso,geppo,soru we will buy this in first
+        Legendary_Swords = true, -- true/false buy all leg sword
+        Ectoplasm_Items = false, -- true/false buy all ecto items
+    },
+    AutoItems = {
+        ["Second Sea"] = {"Kabucha","Rengoku"},
+        ["Third Sea"] = {"Cursed Dual Katana","Soul Guitar","Tushita","Yama","AutoElite","AutoRipIndra"},
+        --remove rainbow haki or any thing if u dont want
+    },
+    Deep_Config = {
+        -- Delay go to Third Sea
+        ["Ectoplasm Farm"] = {
+            Enabled = true, --true/false enabled is needable if u want Soul Guitar
+            FarmUntil = 250, -- 250 = soul guitar price
+        },
+        ["Dark Beard Drop Farm"] = false, -- enabled this if u want 100% get Soul Guitar
+        ["Full 3 Leg Sword and True Triple Katana"] = false, -- enabled this if u want 100% get true triple katana
+    },
+    HopServerFarm = false, -- true/false why not?
+    StopRaidWhenGetEnoughFrag = 10000, --10k is enough so we will farm faster
+    PVP_FARM = false, -- kill player grind exp fast
+    ShowAccountInfo = false, -- an ui with your account information
 }
 
+_G.BypassTeleport = false -- set it true if u wanna fast tp (idk if bug or not)
 
-
-getgenv().Key = "MARU-ZPCT-HE5G7-63C8-445UB-Q1U3"
-getgenv().id = "918007508935802910"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HohoV2/main/ScriptLoad.lua"))()
