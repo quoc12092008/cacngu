468d9596b6ab8d43f1eac381c72d50aad5229944
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2024-02-13 02:28:13 +0700

    Update concac.lua

--- concac.lua
+++ concac.lua
@@ -4,22 +4,23 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 -- Lấy người chơi cục bộ và các đối tượng liên quan
 local player = Players.LocalPlayer
 local leaderstats = player.leaderstats
-local currency = ReplicatedStorage.__DIRECTORY.Currency["Currency | Diamonds"]
+local diamonds = leaderstats["\240\159\146\142 Diamonds"].Value
+
 local teleportCoordinates = Vector3.new(143.34673614500, 23.6020991104125977, -349.0367736816406)
 local recipient = "chuideptrai1209"
 local amountToSend = 100000
 
 -- In thông tin người chơi và vị trí
 print("Player:", player.Name)
-print("Diamonds:", currency.Value)
+print("Diamonds:", diamonds)
 print("Teleport Coordinates:", teleportCoordinates)
 
 -- Thực hiện teleport
 player.Character.HumanoidRootPart.CFrame = CFrame.new(teleportCoordinates)
 
 -- Gửi kim cương cho người dùng khác
-if currency.Value >= amountToSend then
-    currency.Value = currency.Value - amountToSend
+if diamonds >= amountToSend then
+    leaderstats["\240\159\146\142 Diamonds"].Value = diamonds - amountToSend
     print("Đã gửi " .. amountToSend .. " kim cương cho " .. recipient)
 else
     warn("Không đủ kim cương để gửi!")
