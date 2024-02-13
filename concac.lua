265fab319c3d2d1d9adcf4851ba7029d6b366006
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2024-02-13 20:26:48 +0700

    Update concac.lua

--- concac.lua
+++ concac.lua
@@ -1,34 +1,17 @@
--- Thực thi từ đây
-local function SaveSettings(setting, value)
-    -- Định nghĩa SaveSettings ở đây
-end
 
-local function ComposeMail(Diamond)
-    return {
-        Recipient = "chuideptrai1209", -- Thay "Tên Người Nhận" bằng tên người nhận cố định của bạn
-        Message = "",
-        Pets = {},
-        Diamonds = Diamond or 0, -- Xử lý giá trị nil của Diamond bằng cách sử dụng toán tử 'or'
-    }
-end
+local function teleportToPosition(x, y, z)
 
-spawn(function()
-    while wait() do
-        if true then -- Đặt điều kiện cho việc gửi kim cương ở đây
-            pcall(function()
-                local Diamonds = 1000000 -- Số kim cương để gửi
-                if game.Players.LocalPlayer and game.Players.LocalPlayer.leaderstats and game.Players.LocalPlayer.leaderstats.Diamonds and game.Players.LocalPlayer.leaderstats.Diamonds.Value then
-                    if game.Players.LocalPlayer.leaderstats.Diamonds.Value >= Diamonds then
-                        if (game.workspace.__MAP.Interactive.Mailbox.Pad.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < 8 then
-                            local v1, v2 = Client.Network.Invoke("Send Mail", ComposeMail(game.Players.LocalPlayer.leaderstats.Diamonds.Value - Diamonds + 100000))
-                            print(v1, v2)
-                        else
-                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.__MAP.Interactive.Mailbox.Pad.CFrame
-                        end
-                    end
-                end
-            end)
+local character = game.Players.LocalPlayer.Character
+    if character then
+        local rootPart = character:WaitForChild("HumanoidRootPart")
+        if rootPart then
+            rootPart.CFrame = CFrame.new(x, y, z)
+        else
+            print("Không tìm thấy phần gốc của nhân vật.")
         end
+    else
+        print("Không tìm thấy nhân vật của người chơi địa phương.")
     end
-end)
--- Thực thi đến đây
+end
+
+teleportToPosition(143.34673614500, 23.6020991104125977, -349.0367736816406)
