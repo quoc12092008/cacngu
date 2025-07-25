9a434c6f2f9a12aa36009314066910d61f70bfc5
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-07-25 17:55:07 +0700

    Create track.lua

---
+++ track.lua
@@ -0,0 +1,2 @@
+_G.Config = { UserID = "5887ba0e-c847-48e8-8998-d955a6b7c083", discord_id = "1378019320126509147" , Note = "Pc", } 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
