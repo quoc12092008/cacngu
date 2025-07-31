ff0fb3a0e552146cbcc8c462ae143bf19cbcf4fa
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-07-31 23:42:45 +0700

    Update concac.lua

--- concac.lua
+++ concac.lua
@@ -68,7 +68,243 @@ task.delay(60, function()
     setfpscap(2)
 end)
 
-getgenv().ConfigsKaitun = {
+if Tienhienco2 < 1000000000 then
+	getgenv().ConfigsKaitun = {
+	["Block Pet Gift"] = true,
+	["Low Cpu"] = true,
+	["Auto Rejoin"] = false,
+	["Rejoin When Update"] = true,
+	["Limit Tree"] = {
+		["Limit"] = 500,
+		["Destroy Untill"] = 400,
+
+		["Safe Tree"] = {
+			"Elder Strawberry",
+			"Beanstalk",
+			"Ember Lily",
+			"Sugar Apple",
+			"Burning Bud",
+			"Giant Pinecone",
+			"Fossilight",
+			"Maple Apple",
+			"Elephant Ears",
+			"Sunflower",
+			"Dragon Pepper",
+			"Bone Blossom",
+			"Tranquil Bloom",
+			["Tomato"] = 10, ["Strawberry"] = 10, ["Blueberry"] = 10,
+			["Orange Tulip"] = 10, ["Corn"] = 10, ["Daffodil"] = 10,
+			["Bamboo"] = 10, ["Apple"] = 10, ["Coconut"] = 10,
+			["Pumpkin"] = 10, ["Watermelon"] = 10, ["Cactus"] = 10,
+			["Dragon Fruit"] = 10, ["Mango"] = 10, ["Grape"] = 10,
+			["Mushroom"] = 10, ["Pepper"] = 10, ["Cacao"] = 10,
+			["Monoblooma"] = 10, ["Serenity"] = 10, ["Taro Flower"] = 10,
+			["Zen Rocks"] = 10, ["Hinomai"] = 10,
+		}
+	},
+
+	Seed = {
+		Buy = {
+			Mode = "Auto", -- Custom , Auto
+			Custom = {
+
+			}
+		},
+		Place = {
+			Mode = "Lock", -- Select , Lock
+			Select = {
+
+			},
+			Lock = {
+				
+			}
+		}
+	},
+
+	["Seed Pack"] = {
+		Locked = {
+
+		}
+	},
+
+	Events = {
+		["Traveling Shop"] = {
+			"Bald Eagle",
+			"Night Staff",
+			"Star Caller",
+			"Bee Egg",
+			
+		},
+		Craft = {
+			"Ancient Seed Pack",
+			"Primal Egg",
+			"Anti Bee Egg",
+			"Lightning Rod",
+
+		},
+		Shop = {
+			"Zen Egg",
+			"Koi",
+			"Pet Shard Tranquil",
+			"Pet Shard Corrupted",
+			"Flower Seed Pack",
+			"Bee Egg",
+			"Oasis Egg",
+		},
+		["Zen Event"] = {
+			["Restocking"] = { -- Minimumthing to restock
+				Max_Restocks_Price = 4400000000,
+				Minimum_Money = 10000000000,
+				Minimum_Chi = 500
+			},
+			["Doing"] = {
+				Minimum_Money = 50000000, -- minimum money to start play this event
+				First_Upgrade_Tree = 4,
+				Maximum_Chi = 500,
+				Skip_Fox = false,
+				Skip_Corrupted_OldMan = false,
+			}
+		},
+		Start_Do_Honey = 20000000
+	},
+
+	Gear = {
+		Buy = {
+			"Medium Treat",
+			"Medium Toy",
+			"Tanning Mirror",  
+			"Master Sprinkler",
+			"Basic Sprinkler",
+			"Godly Sprinkler",
+			"Advanced Sprinkler",
+			"Lightning Rod",
+		},
+		Lock = {
+
+		},
+	},
+
+	Eggs = {
+		Place = {
+			"Common Summer Egg",
+			"Rare Egg",
+			"Rare Summer Egg",
+			"Corrupted Zen Egg",
+			"Zen Egg",
+			"Primal Egg",
+			"Dinosaur Egg",
+            "Oasis Egg",
+			"Anti Bee Egg",
+            "Paradise Egg",
+            "Night Egg",
+            "Bug Egg",
+            "Exotic Bug Egg",
+            "Mythical Egg",
+
+		},
+		Buy = {
+			"Zen Egg",
+			"Dinosaur Egg",
+            "Oasis Egg",
+			"Bee Egg",
+			"Anti Bee Egg",
+            "Paradise Egg",
+            "Night Egg",
+            "Bug Egg",
+            "Exotic Bug Egg",
+            "Mythical Egg",
+		}
+	},
+
+	Pets = {
+		["Start Delete Pet At"] = 40,
+		["Upgrade Slot"] = {
+			["Pet"] = {
+				["Starfish"] = { 5, 75, 1},
+			},
+			["Limit Upgrade"] = 5,
+			["Equip When Done"] = {
+				["Starfish"] = { 1, 75, 1},
+				["Tanchozuru"] = { 3, 999, 2},
+				["Corrupted Kitsune"] = { 99, 999, 3},
+				["Corrupted Kodama"] = { 3, 999, 4},
+				["Bald Eagle"] = { 99, 999, 5},
+				["Blood Kiwi"] = { 99, 999, 6},
+				["Rooster"] = { 99, 999, 7},
+				["Chicken"] = { 99, 999, 8},
+				["Seal"] = { 99, 999, 9},
+				["Koi"] = { 99, 999, 10},
+				["Pachycephalosaurus"] = { 99, 999, 11},
+				["Orangutan"] = { 99, 999, 12},
+
+			},
+		},
+		Locked = {
+			["Corrupted Kodama"] = 10,
+			["Tanchozuru"] = 3,
+			["Koi"] = 16,
+			"Kitsune",
+			"Corrupted Kitsune",
+			["Kodama"] = 1,
+			["Kappa"] = 1,
+			["Mizuchi"] = 1,
+			["Dilophosaurus"] = 1,
+			["Ankylosaurus"] = 1,
+			"Spinosaurus",
+			["Starfish"] = 8,
+			["Rooster"] = 10,
+			"T-Rex",
+			["Brontosaurus"] = 1,
+			["Pterodactyl"] = 1,
+			["Pachycephalosaurus"] = 10,
+			["Seal"] = 10,
+			["Orangutan"] = 10,
+			["Bald Eagle"] = 8,
+			["Chicken"] = 8,
+			["Moon Cat"] = 1,
+			"Fennec Fox",
+			"Disco Bee",
+			"Butterfly",
+			"Mimic Octopus",
+			"Queen Bee",
+			"Dragonfly",
+			"Raccoon",
+			"Red Fox",
+			["Blood Kiwi"] = 10,
+		},
+		Locked_Pet_Age = 60,
+		LockPet_Weight = 7,
+		Instant_Sell = {
+			"Dog",
+			"Bunny",
+			"Golden Lab",
+		}
+	},
+
+	Webhook = {
+		UrlPet = "https://discordapp.com/api/webhooks/1378811253765574767/t5lFqOqiM641yFiPN6_GJpiTlzzY3m2UIMIH7g9Jye_lfZUIyXkPQum5IiwPmRWbp7pe",
+		UrlSeed = "https://discordapp.com/api/webhooks/1378811068637380658/WxtqHVdfaDsKGkLv8KXTO_0Drynd3LsnELsmtUyb5Jfeu0Ywp4G9ReL30UUmJ8nR1s_p",
+		PcName = "PC",
+
+		Noti = {
+			Seeds = {
+				"Sunflower",
+				"Dragon Pepper",
+				"Elephant Ears",
+			},
+			SeedPack = {
+				"Idk",
+			},
+			Pets = {
+				"Kitsune",
+				"Corrupted Kitsune",
+			},
+			Pet_Weight_Noti = false,
+		}
+	},
+}
+else
+	getgenv().ConfigsKaitun = {
 	["Block Pet Gift"] = true,
 	["Low Cpu"] = true,
 	["Auto Rejoin"] = false,
