b95bbab51294df3ff9526d8a1865ec8203976333
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-08-11 23:05:53 +0700

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,1 +1,3 @@
-print("123)
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().PET_TRACKER_KEY = "vinhga2008"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/quoc12092008/cacccccc/refs/heads/main/main.lua"))()
