e5440f41dd3ee48f7be309a7bd7321c9717e1f61
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-08-18 22:31:32 +0700

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,1 +1,13 @@
+task.delay(80, function()
+	while true do
+    setfpscap(2)
+	task.wait(15)
+	end
+end)
 
+_G.Config = { 
+  UserID = "5887ba0e-c847-48e8-8998-d955a6b7c083", 
+  discord_id = "1378019320126509147" , 
+  Note = "Pc", 
+} 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
