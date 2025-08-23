2bd97e13f5bddaf34126bc7622024f1c98205c4f
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-08-23 17:44:03 +0700

    Update concac.lua

--- concac.lua
+++ concac.lua
@@ -3,7 +3,7 @@ getgenv().Config = {
     ["Gameplay"] = {
         ["Server Type"] = "Private",
         -- Private Server config
-        ["Collect Cash Cap"] = "2B",
+        ["Collect Cash Cap"] = "10B",
         -- Public Server config
         ["Max Auctioning Multiplier"] = 10,
         ["Lock Base Extra Time"] = 5,
@@ -11,8 +11,7 @@ getgenv().Config = {
     ["Misc"] = {
         ["Kick if Ping above"] = 100000,
         ["Ignore Secret"] = {},
-        ["Kick if FPS below"] = 5,
-        ["Buy Lucky Block"] = {
+        ["Buy Lucky Block"] = {},
             "Secret Lucky Block",
     },
     ["Performance"] = {
@@ -32,14 +31,6 @@ getgenv().Config = {
                 "Candy",
                 "Rainbow",
                 "Bloodrot",
-            },
-            ["Job Job Job Sahur"] = {
-                "Normal",
-                "Lava",
-                "Gold",
-                "Diamond",
-                "Bloodrot",
-                "Rainbow",
             },
             ["Chimpanzini Spiderini"] = {
                 "Normal",
@@ -79,4 +70,4 @@ getgenv().Config = {
         },
     }
 }
-spawn(function() task.wait(30) if not getgenv().scriptLoaded then game.Players.LocalPlayer:Kick("Script load timeout\nKick to prevent animal gets stolen") end end) repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until getgenv().Joebiden
+spawn(function() task.wait(300) if not getgenv().scriptLoaded then game.Players.LocalPlayer:Kick("Script load timeout\nKick to prevent animal gets stolen") end end) repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until getgenv().Joebiden
