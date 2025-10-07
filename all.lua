8a6a77f20f09cd551471c9e730576394817e94d6
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-07 12:13:45 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,1 +1,7 @@
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/concac.lua'))()
+end)
 
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
+end)
