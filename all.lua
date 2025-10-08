d90f88ca62bba5703f6c0025a67c94a5824dafdd
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-08 17:53:41 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,5 +1,13 @@
 spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/concac.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/Config%20Steal.txt'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/HeoBeo.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/LockFPS.lua'))()
 end)
 
 spawn(function()
