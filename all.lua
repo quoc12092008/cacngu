4ccbc37fb4732966a99ff0a4f9bba3d1c27b98d6
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-11 21:11:03 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,5 +1,5 @@
 spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/Config%20Steal.txt'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/rua.lua'))()
 end)
 
 spawn(function()
