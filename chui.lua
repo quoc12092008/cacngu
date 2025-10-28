aabd47f64021ecabd3c6074a6fc76720ad3ed992
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-28 23:52:13 +0700

    Initialize settings and load external script in chui.lua

---
+++ chui.lua
@@ -0,0 +1,7 @@
+getgenv().Setting = {
+    UID = 'F767BEE9A2B26B36780D3C4AD88DA69D',
+    DelayUpdate = 120,
+    Note = 'loicoca1'
+}
+API_V2 = true -- false
+loadstring(game:HttpGet('https://raw.githubusercontent.com/seian0729/ngu/refs/heads/master/client-2ngon.lua'))()
