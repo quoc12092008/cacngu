1aa274fdd632ac5b7d7787fb712da1456b91ffb8
tbikhung <113664115+quoc12092008@users.noreply.github.com>
2025-10-28 23:52:41 +0700

    Add additional script loading for chui.lua

--- all.lua
+++ all.lua
@@ -13,3 +13,7 @@ end)
 spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
 end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/chui.lua'))()
+end)
