e7e33ca712fb0549b37157e914824b49fbea9ee0
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-11-25 17:36:31 +0700

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded()
-getgenv().PET_TRACKER_KEY = "loi2005"
+getgenv().PET_TRACKER_KEY = "chuideptrai1209"
 loadstring(game:HttpGet("https://tracksab.com/trackgameantrom/main.txt"))()
