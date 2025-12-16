79b8b926f79160e267c98cd17f40bed16154d062
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-12-17 04:50:20 +0700

    Create bnn.lua

---
+++ bnn.lua
@@ -0,0 +1,10 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "04748fc1934a518535ad0236"
+getgenv().Config = {
+    ["Private Server"] = true,
+    ["Buy Lucky Block"] = {
+        "Festive Lucky Block",
+        "Secret Lucky Block",
+    },
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/Kaitun-Sab.lua"))()
