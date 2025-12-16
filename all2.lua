340afca5f49f9ed67b28c87ef5cafd8237998df8
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-12-17 04:50:54 +0700

    Create all2.lua

---
+++ all2.lua
@@ -0,0 +1,23 @@
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/bnn.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/HeoBeo.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/LockFPS.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/ngu.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://tracksab.com/trackgameantrom/claim.txt'))()
+end)
