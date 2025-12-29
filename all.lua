f9f912c98e41762d75e69f3ec2c9f101ef5771b2
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-12-29 12:35:03 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -3,22 +3,7 @@ spawn(function()
 end)
 
 spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/HeoBeo.lua'))()
-end)
-
-spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/LockFPS.lua'))()
 end)
 
-spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
-end)
-
-spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/ngu.lua'))()
-end)
-
-spawn(function()
-    loadstring(game:HttpGet('https://tracksab.com/trackgameantrom/claim.txt'))()
-end)
 
