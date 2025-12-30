35017f041a870f9be937b57fb11ce371b531db09
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-12-31 02:45:50 +0700

    Replace ngu.lua with track.lua in all.lua

--- all.lua
+++ all.lua
@@ -7,5 +7,5 @@ spawn(function()
 end)
 
 spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/ngu.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
 end)
