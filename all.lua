8c7436dec5828558206fd1355e28044aec6dcf2c
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2025-12-31 22:06:52 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -6,7 +6,3 @@ spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/LockFPS.lua'))()
 end)
 
-spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
-end)
-
