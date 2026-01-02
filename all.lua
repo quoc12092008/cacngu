94af091ad361eedb54b1bf0d544da7317d9301a7
quoc12092008 <113664115+quoc12092008@users.noreply.github.com>
2026-01-02 23:02:38 +0700

    Update all.lua

--- all.lua
+++ all.lua
@@ -9,3 +9,7 @@ end)
 spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/ngu.lua'))()
 end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/quoc12092008/cacngu/refs/heads/main/track.lua'))()
+end)
